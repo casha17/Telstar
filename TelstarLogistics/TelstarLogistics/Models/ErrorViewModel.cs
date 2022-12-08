d53f9929f6fb5d106dd5c22c3ac352d6c488a318
Casper Hansen <cash@netcompany.com>
2022-12-08 11:02:08 +0100

    New project

---
+++ TelstarLogistics/TelstarLogistics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TelstarLogistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
