16c1cddbc132e7b188777aaaead02a178ad2d8cd
aksm <aksm@netcompany.com>
2022-12-08 16:31:54 +0100

    Test

---
+++ TelstarLogistics/TelstarLogistics/service/GraphingService.cs
@@ -0,0 +1,60 @@
+﻿using Dijkstra.NET.Graph;
+using Dijkstra.NET.Graph.Simple;
+using Dijkstra.NET.ShortestPath;
+
+namespace TelstarLogistics.service;
+
+using Dijkstra.NET;
+using Dijkstra.NET.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+public class GraphingService
+{
+
+    public Graph buildGraph()
+    {
+
+        //Fetch the edges from the database
+        
+        
+        //Fetch the remaining edges from EI and OA API
+        
+
+        // Create a new Graph object
+        var graph = new Graph();
+
+        // Add the nodes to the graph
+        foreach (var node in nodes)
+        {
+            graph.AddNode(node.Id);
+        }
+
+        // Add the edges to the graph
+        
+
+        return graph;
+    }
+
+    public ShortestPathResult getShortestPath(Graph graph, string from, string to, bool useTime)
+    {
+
+        ShortestPathResult path = new ShortestPathResult();
+        
+        if (useTime)
+        {
+            path = graph.Dijkstra(from, to, weight => weight.Time);
+        }
+        else
+        {
+            path= graph.Dijkstra(from, to, weight => weight.Cost);
+        }
+
+        return path;
+    }
+
+
+
+}
