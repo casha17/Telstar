f1a75d52bae68906de2340edd15488384c9c0681
mkp <mkp@netcompany.com>
2022-12-08 13:06:20 +0100

    add destination and edge

---
+++ TelstarLogistics/TelstarLogistics/Models/destination.cs
@@ -0,0 +1,12 @@
+﻿namespace TelstarLogistics.Models
+{
+    public class destination
+    {
+        public string city { get; set; }
+
+        public Guid id { get; set; }
+
+        public List<edge> fromEdges { get; set; }
+        public List<edge> toEdges { get; set; }
+    }
+}
---
+++ TelstarLogistics/TelstarLogistics/Models/edge.cs
@@ -0,0 +1,16 @@
+﻿namespace TelstarLogistics.Models
+{
+    public class edge
+    {
+        public Guid Id { get; set; }
+
+        public string to { get; set; }
+        public string from { get; set; }
+        public double cost { get; set; }
+        public double timeHours { get; set; }
+
+        public destination fromDestination { get; set; }
+        public destination toDestination { get; set; }
+
+    }
+}
