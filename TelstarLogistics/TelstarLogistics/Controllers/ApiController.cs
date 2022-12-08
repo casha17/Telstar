a40fc6e3830d9638aacb8c3977e94c3402d7bb66
aksm <aksm@netcompany.com>
2022-12-08 13:38:30 +0100

    mockapi

--- TelstarLogistics/TelstarLogistics/Controllers/ApiController.cs
+++ TelstarLogistics/TelstarLogistics/Controllers/ApiController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TelstarLogistics.Data;
 
 namespace TelstarLogistics.Controllers;
 
-public class ApiController : Controller
+public class ApiController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    
+    [Route("api/edges")]
+    [HttpGet]
+    public ActionResult GetMockData()
     {
-        return View();
+
+        List<MockData> mockData = new List<MockData>
+        {
+            new MockData
+            {
+                CityFrom = "tunis",
+                CityTo = "tripoli",
+                CostInUSD = 9,
+                TimeInHours = 12
+            }
+        };
+        return Ok(mockData);
     }
 }
--- TelstarLogistics/TelstarLogistics/Data/MockData.cs
+++ TelstarLogistics/TelstarLogistics/Data/MockData.cs
@@ -2,5 +2,8 @@
 
 public class MockData
 {
-    
+    public string CityFrom { get; set; }
+    public string CityTo { get; set; }
+    public decimal CostInUSD { get; set; }
+    public decimal TimeInHours { get; set; }
 }
