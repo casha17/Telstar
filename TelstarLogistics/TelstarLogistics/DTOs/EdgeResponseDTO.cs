5977a97b89f9aab92ca28064d7498f2310cc92ff
Konrad Korus <konr@netcompany.com>
2022-12-08 13:55:52 +0100

    Add response and request DTOs

---
+++ TelstarLogistics/TelstarLogistics/DTOs/EdgeResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TelstarLogistics.DTOs
+{
+    public class EdgeResponseDTO
+    {
+        public string City1 { get; set; }
+        public string City2 { get; set; }
+        public decimal CostInUSD { get; set; }
+        public decimal TimeInHours { get; set; }
+    }
+}
