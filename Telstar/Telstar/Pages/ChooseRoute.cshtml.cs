21bbcc455239eae929a3d3b9a9b914b0681d7dd8
Christian Brok Jacobsen <cbj@netcompany.com>
2022-12-08 21:16:24 +0100

    updated ui with all pages

---
+++ Telstar/Telstar/Pages/ChooseRoute.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telstar.Pages
+{
+    public class ChooseRouteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
