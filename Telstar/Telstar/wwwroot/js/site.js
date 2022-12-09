ff27dc5244e5f68e273fd0145c506bbe38774443
Mads Petersen <mads.m.petersen@gmail.com>
2022-12-09 10:57:28 +0100

    added weight validation

--- Telstar/Telstar/wwwroot/js/site.js
+++ Telstar/Telstar/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const checkInput = () => {
+    var value = document.getElementById("weightInput").value
+    if (value > 39) {
+        document.getElementById("weightInput").value = 39;
+        alert("The specified value have to be below 40")
+    }
+}
